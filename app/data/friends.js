6c87f9b7c5e6d2b987f6d0c55b24318e109b2313
Brandon Fuller <fulleb33@gmail.com>
2017-07-29 20:50:34 -0500

    friend-finder

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Kelly",
+	"photo" : "http://i.imgur.com/M7JqWp1.jpg",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Tom",
+	"photo" : "https://i.chzbgr.com/original/8334924288/hCFACD046/1",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Kevin",
+	"photo" : "http://i.imgur.com/yUdSPIY.jpg",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
+
+
+module.exports = friends;
